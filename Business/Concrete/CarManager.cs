e707cbabf8b577a29e36e6f762cda4b04ca2805e
Melih Karatekin <sefamelihkaratekin@gmail.com>
2021-02-17 21:29:39 +0300

    commit

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,41 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void AddCar(Car car)
+        {
+            if (car.CarName.Length >= 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+            }
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int brandId)
+        {
+            return _carDal.GetAll(c => c.BrandId == brandId);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(c => c.ColorId == colorId);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CarManager carManager = new CarManager(new EfCarDal());
+            List<Car> cars = new List<Car>();
+
+            //carManager.AddCar(new Car { Id = 1, BrandId = 2, ColorId = 3, CarName = "Mercedes", DailyPrice = 100, ModelYear = 2021, Description = "CLA 200" });
+            //carManager.AddCar(new Car { Id = 2, BrandId = 2, ColorId = 5, CarName = "Volvo", DailyPrice = 50, ModelYear = 2021, Description = "S90" });
+            
+            cars = carManager.GetAll();
+            foreach (var car in cars)
+            {
+                Console.WriteLine("Car Name: " + car.CarName + "        Car Model Year: " + car.ModelYear);
+            }
+
+        }
+    }
+}
